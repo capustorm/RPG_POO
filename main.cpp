7c693e1cee3bc0d248cb3bef5b896a30c8d037c2
Michael Arturo Contreras Gilpin <capustorm>
2024-02-18 20:31:56 -0600

    Cambios realizados

--- main.cpp
+++ main.cpp
@@ -3,20 +3,31 @@
 #include "Enemy/Enemy.h"
 
 int main() {
-    Player *player = new Player("Victor", 100, 8, 4, 10);
-    Enemy *enemy = new Enemy("Estudiante de Redes", 50, 5, 2, 5, 10);
-
-    cout << player->toString() << endl;
-    cout << enemy->toString() << endl;
+    auto *player = new Player("True Warrior", 100, 10, 4, 10);
+    auto *enemy = new Enemy("Final Boss", 110, 10, 2, 5, 10);
 
     cout << "=====================" << endl;
 
-    player->doAttack(enemy);
-    enemy->doAttack(player);
+    bool combat = true;
+    int i = 1;
+    while (combat == true) {
+        cout << "\nRound " << i << endl;
+        player->doAttack(enemy);
+        enemy->doAttack(player);
+        cout << player->toString() << endl;
+        cout << enemy->toString() << endl;
 
-    cout << player->toString() << endl;
-    cout << enemy->toString() << endl;
+        if (player->getHealth() <= 0)
+        {
+            cout << "\nYou loose." << endl;
+            combat = false;
+        } else if (enemy->getHealth() <= 0) {
+            cout << "\nYou won." << endl;
+            combat = false;
+        }
 
+        i++;
+    }
     delete player;
     delete enemy;
     return 0;
